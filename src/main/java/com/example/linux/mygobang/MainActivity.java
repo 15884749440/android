9a594755b145b8553fdc74e0fa6d870cd99aa289
15884749440 <64135574+15884749440@users.noreply.github.com>
2020-12-29 09:00:47 +0800

    Add files via upload

---
+++ src/main/java/com/example/linux/mygobang/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.linux.mygobang;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+    private ChessBoardView chessBoardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chessBoardView = (ChessBoardView) findViewById(R.id.boardView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // 再来一局
+        if (id == R.id.action_setting) {
+            chessBoardView.start();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
